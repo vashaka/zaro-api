1f92c62e3966d1fea841fce5042a9e83b17bbd78
David Vashakidze <83545222+vashaka@users.noreply.github.com>
2023-06-09 19:55:32 +0400

    oh my god

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with the domain of your frontend application
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with the domain of your frontend application
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
